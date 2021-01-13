a2e44c2469ae67051a7d78148f2b5a1e37ec96fd
Team-KingPanda <72608447+Team-KingPanda@users.noreply.github.com>
2021-01-13 20:50:00 +0700

    nuke.js

---
+++ Tutorial-5/commands/admin/nuke.js
@@ -0,0 +1,25 @@
+const discord = require('discord.js')
+
+module.exports = {
+name: "nuke",
+category: "admin",
+description: "To clear all message on the channel",
+usage: "nuke",
+aliases: ["nukechannel", "clearall"],
+run: async (client, message, args) => {
+
+ if(!message.member.hasPermission("ADMINISTRATOR")) {
+ return message.channel.send(`${message.author}, You are not have permission to use this command!!`);
+ }
+ 
+ let channel = client.channels.cache.get(message.channel.id)
+ var posisi = channel.position;
+
+ channel.clone().then((channel2) => {
+ channel2.setPosition(posisi)
+ channel.delete()
+ channel2.send("This channel has been nuked!!").then(m => m.delete({timeout: 10000}))
+ });
+
+ }
+};
