7bfb30015897c2a37f24a6a62b89ed5fbe1ed2a8
Team-KingPanda <72608447+Team-KingPanda@users.noreply.github.com>
2021-03-07 11:53:31 +0700

    Update server.js

--- Tutorial-6/server.js
+++ Tutorial-6/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const server = express();
 
 server.use('/', (req, res)=>{
-  res.send('Warrior-Panda Now already online.')
+  res.send('Tutorial-Bot-Discord is online.')
 })
 function keepAlive(){
-  server.listen(5555, ()=>{console.log("Server is ready!")
+  server.listen(3000, ()=>{console.log("Server is ready!")
   });
 }
 module.exports = keepAlive;
